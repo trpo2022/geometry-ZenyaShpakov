4b84b6a0179d3aebf29e30ec9bb20d554e63e743
ZenyaShpakov <ZenyaShpakov@users.noreply.github.com>
2022-03-20 16:10:08 +0700

    Use CodeStyle

--- src/statlib/shapes.cpp
+++ src/statlib/shapes.cpp
@@ -15,17 +15,10 @@ void CircleR(float Arr_Circle[])
 
 void TriangleR(float Tria_Point[])
 {
-    float Perimetr = 0, storona_a = 0, storona_b = 0, storona_c = 0, Square = 0,
-          p;
-    storona_a
-            = sqrt(powf(Tria_Point[0] - Tria_Point[2], 2)
-                   + powf(Tria_Point[1] - Tria_Point[3], 2));
-    storona_b
-            = sqrt(powf(Tria_Point[0] - Tria_Point[4], 2)
-                   + powf(Tria_Point[1] - Tria_Point[5], 2));
-    storona_c
-            = sqrt(powf(Tria_Point[2] - Tria_Point[4], 2)
-                   + powf(Tria_Point[3] - Tria_Point[5], 2));
+    float Perimetr = 0, storona_a = 0, storona_b = 0, storona_c = 0, Square = 0, p;
+    storona_a = sqrt(powf(Tria_Point[0] - Tria_Point[2], 2) + powf(Tria_Point[1] - Tria_Point[3], 2));
+    storona_b = sqrt(powf(Tria_Point[0] - Tria_Point[4], 2) + powf(Tria_Point[1] - Tria_Point[5], 2));
+    storona_c = sqrt(powf(Tria_Point[2] - Tria_Point[4], 2) + powf(Tria_Point[3] - Tria_Point[5], 2));
     Perimetr = storona_a + storona_b + storona_c;
     p = Perimetr / 2;
     Square = sqrt(p * (p - storona_a) * (p - storona_b) * (p - storona_c));
