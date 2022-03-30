c9b0ae01309e6c0ce94a1ef8327cdc49942a5fad
ZenyaShpakov <ZenyaShpakov@users.noreply.github.com>
2022-03-30 18:05:56 +0700

    Delete superfluous

--- src/statlib/shapes.cpp
+++ src/statlib/shapes.cpp
@@ -10,11 +10,10 @@ double *CircleR(double Arr_Circle[])
     double PerimCir, SquareCir;
     PerimCir = 2 * Arr_Circle[2] * pi;
     SquareCir = pi * Arr_Circle[2] * Arr_Circle[2];
-    printf("Perimetr of circle: %f\n", PerimCir);
-    printf("Squre of circle: %f\n", SquareCir);
+   // printf("Perimetr of circle: %f\n", PerimCir);
+   // printf("Squre of circle: %f\n", SquareCir);
     Array_For_Shapes[0] = PerimCir;
     Array_For_Shapes[1] = SquareCir;
-    printf("Perimeter of circl e%f\n", Array_For_Shapes[0]);
     return Array_For_Shapes;
     delete Array_For_Shapes;
 }
@@ -29,10 +28,10 @@ double *TriangleR(double Tria_Point[])
     PerimetrTR = storona_a + storona_b + storona_c;
     p = PerimetrTR / 2;
     SquareTR = sqrt(p * (p - storona_a) * (p - storona_b) * (p - storona_c));
-    printf("Perimetr of triangel: %f", PerimetrTR);
-    printf("\nSquare of triangel: %f\n", SquareTR);
+   // printf("Perimetr of triangel: %f", PerimetrTR);
+   // printf("\nSquare of triangel: %f\n", SquareTR);
     Array_For_Shapes[0] = PerimetrTR;
     Array_For_Shapes[1] = SquareTR;
     return Array_For_Shapes;
-    delete Array_For_Shapes;sqrt(powf(Tria_Point[0] - Tria_Point[2], 2) + powf(Tria_Point[1] - Tria_Point[3], 2));
-}
+    delete Array_For_Shapes;
+}    
