3cef8aec40d3132c0f528c6bc39a6f4a6b6fddf2
ZenyaShpakov <ZenyaShpakov@users.noreply.github.com>
2022-03-30 18:02:38 +0700

    Correcting an incorrect test

--- test/shapes_test.cpp
+++ test/shapes_test.cpp
@@ -36,17 +36,17 @@ CTEST(arithmetic_suite, test2)
 }
 CTEST(arithmetic_suite, test3)
 {	
-	double Tria_Point[6] = {-3.0, -2.0, -1.0, 0.0, -3.0, -2.0};
+	double Tria_Point[6] = {-3.0, -2.0, -1.0, 0.0, -3.0, 2.0};
 	double* Tir = TriangleR(Tria_Point);
-	double result = Tir[0];
-	double expected = 5.656855;
-	ASSERT_DBL_NEAR(expected, result);
+	//double result = Tir[0];
+	double expected = 9.656855;
+	ASSERT_DBL_NEAR(expected, Tir[0]);
 }
 CTEST(arithmetic_suite, test4)
 {
-	double Tria_Point[6] = {-3.0, -2.0, -1.0, 0.0, -3.0, -2.0};
+	double Tria_Point[6] = {-3.0, -2.0, -1.0, 0.0, -3.0, 2.0};
 	double* Tir = TriangleR(Tria_Point);
-	double result = Tir[1];
-	double expected = 0.000001;
-	ASSERT_DBL_NEAR(expected, result);
+	//double result = Tir[1];
+	double expected = 4.000001;
+	ASSERT_DBL_NEAR(expected, Tir[1]);
 }
